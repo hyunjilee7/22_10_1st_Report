8baf4294785aebf6aed92a8d38d1611b86e9530c
hyunjilee7 <112398271+hyunjilee7@users.noreply.github.com>
2022-10-09 14:17:20 +0900

    Add files via upload

---
+++ 파이썬 과제 1번.py
@@ -0,0 +1,6 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("환영합니다. \n 파이썬의 세계에 오신 것을 환영합니다. \n 파이썬은 강력합니다.")
+환영합니다. 
+ 파이썬의 세계에 오신 것을 환영합니다. 
+ 파이썬은 강력합니다.
---
+++ 파이썬 과제 2번.py
@@ -0,0 +1,8 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("반갑습니다. 파이썬!")
+반갑습니다. 파이썬!
+>>> print(2*3/10)
+0.6
+>>> print("Hello","world","!!!")
+Hello world !!!
